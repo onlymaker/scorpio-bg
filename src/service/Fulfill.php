c2abbe9d1d1539ac377d0e21d1c900826cffa2ca
jibo <jibo@outlook.com>
2019-10-21 08:20:07 +0800

    Update fulfill receivers

--- src/service/Fulfill.php
+++ src/service/Fulfill.php
@@ -59,7 +59,8 @@ class Fulfill extends \Prefab
             $content .= $line;
         }
         $smtp->set('From', 'service@onlymaker.com');
-        $smtp->set('To', '<jibo@onlymaker.com>');
+        $smtp->set('To', '<pino@onlymaker.com>');
+        $smtp->set('Bcc', '<jibo@onlymaker.com>');
         $smtp->set('Content-Type', 'text/html; charset=UTF-8');
         $smtp->set('Subject', 'Fulfill Notification');
         writeLog('Send out result: ' . $smtp->send($content));
